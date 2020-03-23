704e3b1d336db330bba997fb66639711db7c7cf3
Nazim Rizvic <nazim.rizvic@me.com>
2020-03-23 23:38:04 +1100

    adding functions for ac button, delete btn and pariod btn

--- javascript.js
+++ javascript.js
@@ -50,19 +50,73 @@ operate = (operator,num1,num2) => {
 
 // Button Variables
 
-let oneBtn = document. 
+let zeroBtn = document.getElementById("zero");
+let oneBtn = document.getElementById("one");
+let twoBtn = document.getElementById("two");
+let threeBtn = document.getElementById("three");
+let fourBtn = document.getElementById("four");
+let fiveBtn = document.getElementById("five");
+let sixBtn = document.getElementById("six");
+let sevenBtn = document.getElementById("seven");
+let eightBtn = document.getElementById("eight");
+let nineBtn = document.getElementById("nine");
+
+let periodBtn = document.getElementById("period");
+let allClearBtn = document.getElementById("allClear");
+let clearBtn = document.getElementById("clear");
+let deleteBtn = document.getElementById("delete");
+
+let display = document.getElementById('display');
+
+let numBtn = document.getElementsByClassName("numBtn");
+let opBtn = document.getElementsByClassName("opBtn");
+
+let displayValue = "0"
+let pendingValue;
+let evalStringArray = [];
+
 
 // display functions
 
-let display = document.getElementById('display');
-let displayedContent = document.createTextNode("0");
-display.appendChild(displayedContent);
+let displayUpdate = (clickObj) => {
+    let btnOutput = clickObj.target.innerText;
 
-let seven = document.getElementById('seven');
+    if(displayValue === "0")
+        displayValue = "";
+
+    displayValue += btnOutput;
+    display.innerText = displayValue;
+}
 
 // Event listeners
 
-document.getElementById("seven").addEventListener("click", )
+for (let i = 0; i < numBtn.length; i++) {
+    numBtn[i].addEventListener('click', displayUpdate, false);
+}
+
+// for (let i = 0; i < opBtn.length; i++) {
+//     opBtn[i].addEventListener('click', initiateOperator, false);
+// }
 
 
+allClearBtn.onclick = () => {
+    displayValue = "0";
+    pendingValue = undefined;
+    evalStringArray = [];
+    display.innerHTML = displayValue;
+}
 
+deleteBtn.onclick = () => {
+    let displayValueLength = displayValue.length;
+    displayValue = displayValue.slice(0, displayValueLength - 1);
+    if (displayValue === "")
+    displayValue = "0";
+    display.innerText = displayValue;
+
+}
+
+periodBtn.onclick = () => {
+    if (!displayValue.includes("."))
+        displayValue += ".";
+    display.innerText = displayValue;
+}
